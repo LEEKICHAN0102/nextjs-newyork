0260e62e87b1ced01d96b1057e3d5d6f97aa05b2
Kichan <rlcks01537@gmail.com>
2023-06-22 16:34:17 +0900

    Initialize

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar(){
+  const router=useRouter();
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <style jsx global>{`
+        
+      `}</style>
+    </nav>
+  )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+
+
+export default function About(){
+  return (
+    <div>
+      <h1>About us</h1>
+      <span>Welcome to the official explorer for the New York Times Best Seller List explorer</span>
+      <span>We Hope you enjoy your stay!</span>
+    </div>
+  )
+}
